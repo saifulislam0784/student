db94bb3579c8acff8204d6b5773b8a8fecc9a091
saifulislam <saifulislam0784@gmail.com>
2020-09-23 18:57:06 +0600

    Database form design

---
+++ index.php
@@ -0,0 +1,100 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Development Area</title>
+	<!-- ALL CSS FILES  -->
+	<link rel="stylesheet" href="assets/css/bootstrap.min.css">
+	<link rel="stylesheet" href="assets/css/style.css">
+	<link rel="stylesheet" href="assets/css/responsive.css">
+</head>
+<body>
+	
+	
+
+	<div class="wrap shadow">
+		<div class="card">
+			<div class="card-body">
+				<h2>Sign Up</h2>
+				<form action="">
+
+					<div class="form-group">
+						<input class="form-control" type="text" placeholder="Name">
+					</div>
+
+					<div class="form-group">
+						<input class="form-control" type="text" placeholder="Email">
+					</div>
+
+					<div class="form-group">
+						<input class="form-control" type="text" placeholder="Cell">
+					</div>
+
+					<div class="form-group">
+						<input class="form-control" type="text" placeholder="Username">
+					</div>
+
+                    <div class="form-group">
+                        <label for="">Gender</label> <br>
+                        <input name="gender" class="" type="radio" id="male"><label for="male">Male</label>
+                        <input name="gender" class="" type="radio" id="female"><label for="male">Female</label>
+                    </div>
+                    <div class="form-group">
+                        <input class="form-control" type="text" placeholder="Age">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="">Shift</label>
+                        <select class="form-control" name="" id="">
+                            <option value="">-select-</option>
+                            <option value="">Morning</option>
+                            <option value="">Day</option>
+                        </select>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="">Location</label>
+                        <select class="form-control" name="" id="">
+                            <option value="">-select-</option>
+                            <option value="">Barisal</option>
+                            <option value="">Chittagong</option>
+                            <option value="">Dhaka</option>
+                            <option value="">Khulna</option>
+                            <option value="">Mymensingh</option>
+                            <option value="">Rajshahi</option>
+                            <option value="">Rangpur</option>
+                            <option value="">Sylhet</option>
+                        </select>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="">Image</label>
+                        <input class="form-control" type="file">
+                    </div>
+
+					<div class="form-group">
+						<input class="btn btn-primary" type="submit" value="Sign Up">
+					</div>
+				</form>
+			</div>
+		</div>
+	</div>
+	
+
+
+
+
+
+
+
+
+
+	<!-- JS FILES  -->
+	<script src="assets/js/jquery-3.4.1.min.js"></script>
+	<script src="assets/js/popper.min.js"></script>
+	<script src="assets/js/bootstrap.min.js"></script>
+	<script src="assets/js/custom.js"></script>
+</body>
+</html>
+
+
